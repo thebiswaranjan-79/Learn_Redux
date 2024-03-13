7be280623d35da4acbbbfeb5e8d1df4fbd28fb13
Biswaranjan <mbiswaranjan797@gmail.com>
2024-03-13 23:56:05 +0530

    Learn Subscribe Method to aware about any further changes

--- createStore-Demo.js
+++ createStore-Demo.js
@@ -6,20 +6,23 @@ function demoReducer(state, action){
     return state;
 }
 
+// These function's are called as Action Creators
 const add_item = (name, quantity) => ({
     type : 'add_item',
     itemName : name, 
     itemQuantity : ((quantity) ? quantity : 1)
 });
 
-const initialState = [{name : 'apple'}, {name : 'rice'}];
+const initialState = [{name : 'apple', quantity:6}, {name : 'rice'}];
 const store = createStore(demoReducer, initialState);
+store.subscribe(()=> console.log("State might Change"));  
+
 console.log(store.getState());
 
 // the only way to Update the Store is by dispatching an action 
 store.dispatch(add_item('Banana'));
 
-store.dispatch(add_item('Mango'));
+store.dispatch(add_item('Mango', 4));
 
 // It is Problematic when we change one line by line ode in every dispatch mthod 
 console.log(store.getState());
