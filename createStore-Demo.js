ff7d84a2569ddac2ab9f0d68c369d03e66f29ab3
Biswaranjan <mbiswaranjan797@gmail.com>
2024-03-13 23:30:41 +0530

    Dispatch Method Solution

--- createStore-Demo.js
+++ createStore-Demo.js
@@ -1,27 +1,25 @@
 import { createStore } from "redux";
 function demoReducer(state, action){
     if(action.type == 'add_item'){
-        return [...state ,{name : action.itemName, quantity : action.quantity}]
+        return [...state ,{name : action.itemName, quantity : action.itemQuantity}]
     }
     return state;
 }
+
+const add_item = (name, quantity) => ({
+    type : 'add_item',
+    itemName : name, 
+    itemQuantity : ((quantity) ? quantity : 1)
+});
 
 const initialState = [{name : 'apple'}, {name : 'rice'}];
 const store = createStore(demoReducer, initialState);
 console.log(store.getState());
 
 // the only way to Update the Store is by dispatching an action 
-store.dispatch({
-    type : 'add_item',
-    itemName : 'Banana',
-    quantity : 1,
-});
+store.dispatch(add_item('Banana'));
 
-store.dispatch({
-    type : 'add_item',
-    itemName : 'Mango',
-    quantity : 2,
-});
+store.dispatch(add_item('Mango'));
 
 // It is Problematic when we change one line by line ode in every dispatch mthod 
 console.log(store.getState());
