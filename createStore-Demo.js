7e36612fbf8fc56c317ad2e66c34f90620182360
Biswaranjan <mbiswaranjan797@gmail.com>
2024-03-15 20:43:07 +0530

    Learn about Reducers

--- createStore-Demo.js
+++ createStore-Demo.js
@@ -1,4 +1,4 @@
-import { createStore } from "redux";
+import { createStore, bindActionCreators } from "redux";
 function demoReducer(state, action){
     if(action.type == 'add_item'){
         return [...state ,{name : action.itemName, quantity : action.itemQuantity}]
@@ -17,12 +17,20 @@ const initialState = [{name : 'apple', quantity:6}, {name : 'rice'}];
 const store = createStore(demoReducer, initialState);
 store.subscribe(()=> console.log("State might Change"));  
 
+
+const actions = bindActionCreators({add_item}, store.dispatch);
+console.log(actions);
 console.log(store.getState());
 
-// the only way to Update the Store is by dispatching an action 
-store.dispatch(add_item('Banana'));
 
-store.dispatch(add_item('Mango', 4));
+actions.add_item('Banana',10);
+// the only way to Update the Store is by dispatching an action 
+// store.dispatch(add_item('Banana'));
+
+// store.dispatch(add_item('Mango', 4));
 
 // It is Problematic when we change one line by line ode in every dispatch mthod 
 console.log(store.getState());
+
+// Actiobn Creators are the functoin which is used to 
+// bindActionCreators(actionCreators, dispatch)         
